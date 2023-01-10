0abd0e34a3ec76f50fa27ce8d970508470fd3a49
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-10 16:23:41 -0500

    Add content to Home page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <nav className="relative w-full flex flex-wrap items-center justify-start py-4 bg-red-100 shadow-lg navbar navbar-expand-lg navbar-light">
-            <h1 className='text-4xl pl-20 w-2/3'>Shopping Cart</h1>
+        <nav className="relative w-full flex flex-wrap items-center justify-start py-4 bg-slate-300 shadow-lg navbar navbar-expand-lg navbar-light">
+            <h1 className='text-4xl pl-20 w-2/3'>Controllify</h1>
             <div className="flex justify-around pr-20 w-1/3">
                 <Link to='/'><h2 className="hover:cursor-pointer">Home</h2></Link>
                 <Link to='/store'><h2 className="hover:cursor-pointer">Store</h2></Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
     return (
-        <div>
-            <h1>This is home!</h1>
+        <div className="flex flex-col items-center bg-gradient-to-r from-purple-500 to-pink-500 h-screen">
+            <h1 className="text-4xl font-bold my-20">Welcome to Controllify!</h1>
+            <p className="text-xl mb-20">Get your hottest gaming controllers here at Controllify for the best prices!</p>
+            <p className="text-xl">Fast deliveries and sweet deals!</p>
         </div>
     )
 }
