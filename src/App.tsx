054a2297815efb91bb7a85153c6734d3f4e1bedd
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-10 16:43:20 -0500

    Add cart to state and addToCart method

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Checkout from "./pages/Checkout";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import './styles.css';
+import { v4 as uuidv4 } from "uuid";
+
+type CartItem = {
+  id: string;
+  name: string;
+  price: number;
+  src: string;
+}
 
 const App = () => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const addToCart = () => {
+    setCart([...cart, {
+      id: uuidv4(),
+      name: 'hi',
+      price: 54,
+      src: 'bye',
+    }])
+  };
+
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/store' element={<Store />} />
+        <Route path='/store' element={<Store cart={cart} addToCart={addToCart} />} />
         <Route path='/checkout' element={<Checkout />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,4 +1,9 @@
-const Store = () => {
+type StoreProps = {
+    cart: Object[]
+    addToCart: () => void
+}
+
+const Store = (props: StoreProps) => {
     return (
         <div className="flex justify-center bg-gradient-to-r from-purple-500 to-pink-500 h-screen">
             <h1 className="text-4xl font-bold my-20">Products</h1>
